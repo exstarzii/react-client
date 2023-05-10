ef25a3dc1f57f22dc92c8e87f1ada7b34de1744e
exstarzii <exstarzii@yandex.ru>
2023-05-10 23:11:17 +0300

    root path added

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Signup />}/>
         <Route path="/signup" element={<Signup />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/user" element={<User />}/>
